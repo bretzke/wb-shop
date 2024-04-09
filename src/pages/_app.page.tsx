791f0219aef4dba68dd8b6dedfba285627c677ce
Willian Bretzke <willianbretzke2013@hotmail.com>
2024-04-08 22:02:31 -0300

    Added theme provider

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,11 +1,19 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import DefaultLayout from "@/layout/DefaultLayout";
+import ThemeProvider from "@/components/ThemeProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DefaultLayout>
-      <Component {...pageProps} />
-    </DefaultLayout>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
+    </ThemeProvider>
   );
 }
